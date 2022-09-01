572967b4617827f0a087ea851a70ff8969dcb81c
Redstonerayy <redstone.rayy@gmail.com>
2022-09-01 11:40:44 +0200

    count

--- algorithms/count_list.py
+++ algorithms/count_list.py
@@ -1,36 +1,7 @@
-"""
-Informatik 12
-Daten gleichen Typs speichern und verarbeiten mit einer Reihung
-Material zur Einführung der Datenstruktur „Reihung"
-ZIE, 20.09.2021
-"""
-
-#Reihung twenty_numbers mit 20 Ganzzahlen zwischen 1 und 10 
-from re import sub
-
-
-twenty_numbers=[4, 5, 5, 2, 8, 10, 3, 2, 7, 6, 9, 9, 4, 8, 10, 9, 10, 9, 10, 6]
-
 #Reihung lieblingsfach mit 1.000  Einträgen aus der Fächerliste ["Mathe", "Deutsch", "Englisch", "Informatik", "Sport", "PU"]
 lieblingsfach=['Mathe', 'Informatik', 'Deutsch', 'Englisch', 'Englisch', 'Englisch', 'Informatik', 'Englisch', 'Informatik', 'Englisch', 'Englisch', 'Mathe', 'Informatik', 'Deutsch', 'Deutsch', 'Informatik', 'Englisch', 'PU', 'PU', 'Sport', 'PU', 'Deutsch', 'Sport', 'PU', 'Sport', 'Mathe', 'PU', 'PU', 'Mathe', 'Sport', 'PU', 'Sport', 'Englisch', 'Informatik', 'Informatik', 'Mathe', 'Deutsch', 'PU', 'Sport', 'Informatik', 'PU', 'Informatik', 'Informatik', 'Sport', 'Deutsch', 'Englisch', 'Mathe', 'Deutsch', 'Deutsch', 'Mathe', 'Informatik', 'Mathe', 'Deutsch', 'Deutsch', 'Englisch', 'Englisch', 'Englisch', 'PU', 'Informatik', 'PU', 'PU', 'Informatik', 'PU', 'Informatik', 'Englisch', 'Deutsch', 'Englisch', 'Informatik', 'Deutsch', 'Sport', 'Deutsch', 'Informatik', 'Mathe', 'Deutsch', 'Mathe', 'Mathe', 'Sport', 'PU', 'Deutsch', 'PU', 'PU', 'PU', 'Deutsch', 'Sport', 'PU', 'PU', 'Deutsch', 'Englisch', 'Deutsch', 'Informatik', 'Sport', 'Mathe', 'Mathe', 'Informatik', 'Sport', 'Englisch', 'Sport', 'Mathe', 'Mathe', 'PU', 'Mathe', 'Informatik', 'Deutsch', 'Englisch', 'Mathe', 'Informatik', 'Englisch', 'Informatik', 'Informatik', 'Sport', 'PU', 'Informatik', 'Mathe', 'Informatik', 'Informatik', 'Englisch', 'Mathe', 'Mathe', 'Mathe', 'Mathe', 'Deutsch', 'PU', 'PU', 'Englisch', 'Sport', 'Mathe', 'Deutsch', 'Sport', 'Sport', 'Informatik', 'Deutsch', 'Informatik', 'Deutsch', 'Deutsch', 'Mathe', 'Sport', 'Englisch', 'Englisch', 'Englisch', 'PU', 'Mathe', 'Mathe', 'Sport', 'Mathe', 'Sport', 'Informatik', 'Informatik', 'Sport', 'Englisch', 'Mathe', 'Informatik', 'Mathe', 'Sport', 'PU', 'Informatik', 'Mathe', 'Englisch', 'Sport', 'Sport', 'Englisch', 'Deutsch', 'Englisch', 'PU', 'Informatik', 'Informatik', 'Sport', 'Sport', 'Englisch', 'Informatik', 'Sport', 'PU', 'Mathe', 'Sport', 'Deutsch', 'Mathe', 'Deutsch', 'Sport', 'Deutsch', 'Mathe', 'Mathe', 'PU', 'Informatik', 'Englisch', 'PU', 'PU', 'Mathe', 'Deutsch', 'Englisch', 'Mathe', 'Sport', 'Informatik', 'Informatik', 'Informatik', 'Mathe', 'Mathe', 'Informatik', 'PU', 'PU', 'Deutsch', 'Informatik', 'Deutsch', 'Sport', 'Deutsch', 'Englisch', 'Informatik', 'Deutsch', 'Sport', 'Deutsch', 'Englisch', 'Deutsch', 'Deutsch', 'Sport', 'Sport', 'Mathe', 'Sport', 'Informatik', 'Deutsch', 'Englisch', 'Deutsch', 'Deutsch', 'Informatik', 'Sport', 'Mathe', 'Deutsch', 'PU', 'PU', 'PU', 'Informatik', 'Mathe', 'Englisch', 'Informatik', 'Deutsch', 'PU', 'Englisch', 'Englisch', 'Englisch', 'Sport', 'Deutsch', 'Deutsch', 'Sport', 'Sport', 'Englisch', 'Sport', 'Mathe', 'Sport', 'Informatik', 'Englisch', 'Englisch', 'Informatik', 'Deutsch', 'Sport', 'Sport', 'Sport', 'Englisch', 'Deutsch', 'PU', 'PU', 'Englisch', 'Sport', 'PU', 'Sport', 'PU', 'Englisch', 'Sport', 'Deutsch', 'Sport', 'Englisch', 'Informatik', 'Mathe', 'Deutsch', 'Informatik', 'Mathe', 'Informatik', 'PU', 'Englisch', 'Deutsch', 'Sport', 'Deutsch', 'Mathe', 'PU', 'Deutsch', 'Englisch', 'PU', 'Deutsch', 'Sport', 'Informatik', 'Mathe', 'Deutsch', 'Informatik', 'Sport', 'Mathe', 'Sport', 'Informatik', 'PU', 'Informatik', 'Sport', 'Englisch', 'Sport', 'PU', 'Informatik', 'Informatik', 'PU', 'Informatik', 'Informatik', 'Informatik', 'Sport', 'Informatik', 'PU', 'Informatik', 'Englisch', 'Sport', 'Sport', 'Mathe', 'Informatik', 'Englisch', 'PU', 'Deutsch', 'Deutsch', 'Sport', 'Deutsch', 'Mathe', 'Deutsch', 'Informatik', 'Deutsch', 'Deutsch', 'Informatik', 'PU', 'Deutsch', 'Sport', 'Mathe', 'Informatik', 'Englisch', 'Mathe', 'PU', 'Mathe', 'Mathe', 'Englisch', 'Englisch', 'Deutsch', 'Sport', 'Deutsch', 'Deutsch', 'Mathe', 'Informatik', 'PU', 'Sport', 'PU', 'Englisch', 'Informatik', 'PU', 'Deutsch', 'PU', 'Sport', 'Informatik', 'PU', 'Informatik', 'PU', 'Informatik', 'Englisch', 'PU', 'Deutsch', 'Sport', 'Englisch', 'Deutsch', 'PU', 'Englisch', 'PU', 'Sport', 'Deutsch', 'Sport', 'Deutsch', 'Informatik', 'Sport', 'Englisch', 'Mathe', 'Deutsch', 'PU', 'Deutsch', 'Mathe', 'Sport', 'PU', 'Sport', 'Deutsch', 'Mathe', 'Deutsch', 'PU', 'Mathe', 'Deutsch', 'Sport', 'PU', 'Mathe', 'Sport', 'Informatik', 'Sport', 'PU', 'Mathe', 'Englisch', 'Deutsch', 'Deutsch', 'Informatik', 'Deutsch', 'PU', 'Deutsch', 'PU', 'Informatik', 'Deutsch', 'PU', 'Mathe', 'Informatik', 'PU', 'Informatik', 'Sport', 'PU', 'Englisch', 'Englisch', 'Deutsch', 'Informatik', 'Englisch', 'Englisch', 'Sport', 'Mathe', 'Deutsch', 'Mathe', 'Informatik', 'Sport', 'Sport', 'Mathe', 'Englisch', 'Englisch', 'Informatik', 'Informatik', 'Informatik', 'Mathe', 'Sport', 'Sport', 'Sport', 'PU', 'Englisch', 'Englisch', 'PU', 'Mathe', 'Englisch', 'Mathe', 'Sport', 'Deutsch', 'PU', 'PU', 'Informatik', 'Deutsch', 'Sport', 'Informatik', 'Deutsch', 'Deutsch', 'PU', 'Deutsch', 'Mathe', 'Englisch', 'Informatik', 'Englisch', 'Sport', 'Informatik', 'Englisch', 'Deutsch', 'Deutsch', 'Mathe', 'Informatik', 'Sport', 'Deutsch', 'Deutsch', 'Sport', 'Mathe', 'PU', 'Englisch', 'Informatik', 'Mathe', 'Mathe', 'Englisch', 'Deutsch', 'Deutsch', 'Mathe', 'Sport', 'Deutsch', 'Informatik', 'Sport', 'PU', 'PU', 'Mathe', 'Englisch', 'PU', 'PU', 'Sport', 'PU', 'Deutsch', 'Sport', 'PU', 'Informatik', 'Informatik', 'Englisch', 'Deutsch', 'Mathe', 'Mathe', 'Sport', 'Informatik', 'Sport', 'Mathe', 'Sport', 'Deutsch', 'Englisch', 'Englisch', 'Mathe', 'Mathe', 'PU', 'PU', 'Englisch', 'Englisch', 'PU', 'Deutsch', 'Sport', 'Deutsch', 'Englisch', 'Mathe', 'Informatik', 'PU', 'Deutsch', 'Mathe', 'Englisch', 'Mathe', 'Mathe', 'Englisch', 'Sport', 'Deutsch', 'Englisch', 'Englisch', 'Englisch', 'Informatik', 'Sport', 'Sport', 'Mathe', 'Sport', 'Englisch', 'Englisch', 'Informatik', 'Mathe', 'Englisch', 'PU', 'Englisch', 'Englisch', 'PU', 'Sport', 'Sport', 'Deutsch', 'Deutsch', 'Mathe', 'Englisch', 'Sport', 'Englisch', 'Englisch', 'Englisch', 'Mathe', 'PU', 'Englisch', 'Mathe', 'Mathe', 'Informatik', 'Informatik', 'Deutsch', 'Informatik', 'Informatik', 'Englisch', 'Sport', 'Englisch', 'Sport', 'PU', 'Deutsch', 'Deutsch', 'Deutsch', 'PU', 'PU', 'Deutsch', 'Sport', 'Mathe', 'Deutsch', 'Sport', 'PU', 'Sport', 'Sport', 'Deutsch', 'Deutsch', 'Deutsch', 'PU', 'Informatik', 'Informatik', 'Deutsch', 'Sport', 'Englisch', 'Informatik', 'Englisch', 'Deutsch', 'Informatik', 'Deutsch', 'Sport', 'Englisch', 'Sport', 'Mathe', 'PU', 'Deutsch', 'Englisch', 'Sport', 'Informatik', 'Informatik', 'Informatik', 'Deutsch', 'Informatik', 'Sport', 'Deutsch', 'Deutsch', 'Englisch', 'Sport', 'Informatik', 'Englisch', 'Deutsch', 'Englisch', 'Deutsch', 'Mathe', 'Deutsch', 'Deutsch', 'Sport', 'Sport', 'PU', 'Englisch', 'Informatik', 'Informatik', 'Sport', 'Deutsch', 'Mathe', 'Mathe', 'Mathe', 'Sport', 'Deutsch', 'Englisch', 'Sport', 'Deutsch', 'Deutsch', 'PU', 'Englisch', 'Deutsch', 'Informatik', 'Informatik', 'Sport', 'PU', 'Deutsch', 'Deutsch', 'Sport', 'Sport', 'Deutsch', 'Mathe', 'Mathe', 'Informatik', 'Deutsch', 'Sport', 'Sport', 'Englisch', 'Informatik', 'Sport', 'Mathe', 'Deutsch', 'Deutsch', 'PU', 'Englisch', 'Englisch', 'Mathe', 'Informatik', 'Englisch', 'Deutsch', 'Englisch', 'Englisch', 'Deutsch', 'Mathe', 'Englisch', 'Informatik', 'PU', 'PU', 'Mathe', 'Mathe', 'Deutsch', 'Mathe', 'PU', 'Deutsch', 'Deutsch', 'PU', 'Englisch', 'Informatik', 'Mathe', 'Englisch', 'Englisch', 'Sport', 'Mathe', 'Sport', 'Deutsch', 'Englisch', 'PU', 'Mathe', 'Deutsch', 'Englisch', 'Informatik', 'Deutsch', 'Deutsch', 'Mathe', 'Mathe', 'Mathe', 'Mathe', 'Informatik', 'Sport', 'Mathe', 'Sport', 'Sport', 'Sport', 'Informatik', 'Sport', 'Mathe', 'Englisch', 'Deutsch', 'Mathe', 'Informatik', 'Informatik', 'Informatik', 'Informatik', 'Sport', 'PU', 'PU', 'Sport', 'PU', 'Mathe', 'Englisch', 'Sport', 'Informatik', 'PU', 'Deutsch', 'Deutsch', 'Mathe', 'Sport', 'Deutsch', 'Informatik', 'Informatik', 'Sport', 'PU', 'Sport', 'Sport', 'Deutsch', 'Mathe', 'Deutsch', 'Informatik', 'Englisch', 'PU', 'PU', 'Sport', 'PU', 'Informatik', 'Englisch', 'Sport', 'Deutsch', 'Englisch', 'Deutsch', 'Mathe', 'Informatik', 'Informatik', 'Sport', 'Informatik', 'PU', 'Mathe', 'PU', 'Sport', 'Informatik', 'Englisch', 'PU', 'Sport', 'PU', 'Mathe', 'Mathe', 'Sport', 'Deutsch', 'Deutsch', 'Mathe', 'Mathe', 'Sport', 'Deutsch', 'Informatik', 'PU', 'Deutsch', 'Informatik', 'Mathe', 'Mathe', 'Englisch', 'Englisch', 'PU', 'Englisch', 'Mathe', 'Sport', 'Sport', 'Mathe', 'Sport', 'Deutsch', 'Deutsch', 'PU', 'Deutsch', 'Sport', 'Sport', 'Mathe', 'Deutsch', 'Informatik', 'Deutsch', 'Mathe', 'PU', 'Informatik', 'Deutsch', 'Informatik', 'Deutsch', 'PU', 'Mathe', 'Deutsch', 'Informatik', 'Englisch', 'Sport', 'Mathe', 'Englisch', 'Sport', 'Englisch', 'Informatik', 'PU', 'Englisch', 'Informatik', 'Deutsch', 'Informatik', 'PU', 'Deutsch', 'Sport', 'Deutsch', 'PU', 'Englisch', 'Deutsch', 'Informatik', 'Mathe', 'Deutsch', 'Mathe', 'Mathe', 'Mathe', 'Englisch', 'Informatik', 'Englisch', 'Deutsch', 'Sport', 'PU', 'Englisch', 'Deutsch', 'PU', 'Deutsch', 'PU', 'Informatik', 'PU', 'Englisch', 'Informatik', 'Deutsch', 'Deutsch', 'Deutsch', 'PU', 'Englisch', 'Englisch', 'Mathe', 'Sport', 'Deutsch', 'Informatik', 'Informatik', 'Englisch', 'Deutsch', 'Mathe', 'Deutsch', 'Englisch', 'Informatik', 'Informatik', 'PU', 'Englisch', 'Mathe', 'Sport', 'Sport', 'PU', 'PU', 'Mathe', 'Informatik', 'PU', 'Englisch', 'Deutsch', 'Sport', 'Mathe', 'Sport', 'Englisch', 'Deutsch', 'Mathe', 'Deutsch', 'Mathe', 'Sport', 'Englisch', 'Deutsch', 'Sport', 'Englisch', 'Informatik', 'PU', 'Deutsch', 'Informatik', 'PU', 'Deutsch', 'Deutsch', 'Mathe', 'Deutsch', 'PU', 'Informatik', 'Englisch', 'Sport', 'PU', 'Englisch', 'Informatik', 'Informatik', 'Deutsch', 'Informatik', 'Mathe', 'Informatik', 'Mathe', 'Deutsch', 'Sport', 'PU', 'Mathe', 'Deutsch', 'Sport', 'Mathe', 'Englisch', 'Sport', 'Englisch', 'Sport', 'Sport', 'Sport', 'PU', 'PU', 'Englisch', 'Mathe', 'Informatik', 'Mathe', 'Englisch', 'Deutsch', 'Englisch', 'Sport', 'PU', 'Informatik', 'Deutsch', 'PU', 'PU', 'Mathe', 'PU', 'Deutsch', 'Sport', 'Englisch', 'Deutsch', 'Deutsch', 'PU', 'Mathe', 'PU', 'Mathe', 'Deutsch', 'Sport', 'PU', 'PU', 'Englisch', 'Mathe', 'Mathe', 'Englisch', 'Mathe', 'Sport', 'Mathe', 'Deutsch', 'Informatik', 'Sport', 'Sport', 'Mathe', 'PU', 'Mathe', 'PU', 'Informatik', 'Informatik', 'Mathe', 'Mathe', 'Mathe', 'Deutsch', 'Mathe', 'PU', 'PU', 'Englisch', 'Informatik', 'PU', 'Englisch', 'Sport', 'Sport', 'Mathe']
 
-print("Gegebene Daten:")
-# print(twenty_numbers, "\n")
-# print(lieblingsfach, "\n")
-
-#Reihungen können mit for-in-range-Schleifen durchlaufen werden:
-#Beispiel:
-#Es wir nacheinander auf die Werte der Reihung zugegriffen
-#um sie z.B. untersuchen zu können
-#oder um sie wie hier im Beispiel auszugeben.
-#Im ersten Schleifendurchlauf wird auf den ersten Wert (an Position 0) zugegriffen,
-#im zweiten Schleifendurchlauf auf den zweiten Wert (an Position 1) usw.
-# for pos in range(0, len(twenty_numbers)):
-#     wert=twenty_numbers[pos]
-#     print(wert)
-
-
-
+# with dictionary
 subjects = {}
 for i in lieblingsfach:
     if i not in subjects:
@@ -38,12 +9,14 @@ for i in lieblingsfach:
     else: 
         subjects[i] += 1
 
+# print out
 print(len(lieblingsfach))
 print(subjects)
 
 liste = [0] * 256
 namen = [0] * 256
 
+# with ordinals for names with different starting letters
 for i in lieblingsfach:
     liste[ord(i[0])] += 1
     namen[ord(i[0])] = i
@@ -52,6 +25,16 @@ for i in range(len(liste)):
     if liste[i] != 0:
         print(liste[i], namen[i])
 
+# sort list
 lieblingsfach.sort()
-print(lieblingsfach)
-for i in range()
+faecher = []
+counted = 0
+for i in range(len(lieblingsfach)):
+    if i + 1 >= len(lieblingsfach):
+        faecher.append([lieblingsfach[i], i + 1 - counted])
+        break
+    if lieblingsfach[i] != lieblingsfach[i + 1]:
+        faecher.append([lieblingsfach[i], i + 1 - counted])
+        counted += i + 1
+
+print(faecher)
