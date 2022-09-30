93002f9fc96d12b92ea4c43334ddd5fc64baab8f
Redstonerayy <redstone.rayy@gmail.com>
2022-09-30 12:28:10 +0200

    informatik aufgabe

---
+++ stars/main.py
@@ -0,0 +1,12 @@
+from stardiff import *
+from stardiff import checksize
+
+# diff = imagediff(bild1, bild2)
+# printimg(diff)
+# img1data = makeimg(bild1, "bild1.png")
+# img2data = makeimg(bild2, "bild2.png")
+# diffdata = makeimg(diff, "diff.png")
+# joined = [*img1data, *img2data, *diffdata]
+# imgfromdata(joined, 10, 30)
+printimg(bild1)
+checksize(bild1)
---
+++ stars/stardiff.py
@@ -0,0 +1,89 @@
+from PIL import Image
+
+def imagediff(img1, img2):
+	diff = []
+	
+	for i in range(len(img1)):
+		temp = []
+		for j in range(len(img1[0])):
+			if (img1[i][j] != img2[i][j]):
+				temp.append(1)
+			else:
+				temp.append(0)
+			
+		diff.append(temp[:])
+
+	return diff
+
+def printimg(img):
+	for row in img:
+		print(row)
+	
+def makeimg(img, filename):
+	data = []
+	for i in range(len(img)):
+		for j in range(len(img[0])):
+			if img[i][j]:
+				data.append(255)
+			else:
+				data.append(0)
+
+	img = Image.new('L', (len(img[0]), len(img)))
+	img.putdata(data)
+	img.save(filename)
+
+	return data
+
+def imgfromdata(data, x, y):
+	img = Image.new('L', (x, y))
+	img.putdata(data)
+	img.save("joined.png")
+
+def checksize(img):
+	for y in range(len(img)):
+		for x in range(len(img[y])):
+			if img[y][x]:
+				print(y, x)
+				print(checkpixel(img, x, y))
+			else:
+				checkpixel(img, x, y)
+
+
+def checkpixel(img, x, y):
+	count = 0
+	checks = 0
+	ystart = y - 1 if y - 1 > -1 else 0
+	yend = y + 2 if y + 2 < len(img) else len(img)
+	xstart = x - 1 if x - 1 > -1 else 0
+	xend = x + 2 if x + 2 < len(img[y]) else len(img[y])
+	for y in range(ystart, yend):
+		for x in range(xstart, xend):
+			checks += 1
+			if img[y][x]:
+				count += 1
+			
+	return count
+
+
+# img data
+bild1 = [[0,0,0,0,0,0,0,0,0,0],
+         [0,1,0,0,0,0,0,0,0,1],
+         [0,0,0,0,0,1,1,0,0,0],
+         [0,0,0,1,0,1,1,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0],
+         [0,0,1,0,0,0,0,0,0,0],
+         [1,0,0,0,0,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0],
+         [0,0,0,0,0,0,0,1,0,0],
+         [0,1,1,0,0,0,0,0,0,0]]
+
+bild2 = [[0,0,0,0,0,0,0,0,0,0],
+         [0,1,0,0,0,0,0,0,0,1],
+         [0,0,0,0,0,1,1,0,0,0],
+         [0,0,0,1,0,1,1,0,0,0],
+         [0,0,0,0,0,0,0,0,0,0],
+         [0,0,0,1,0,0,0,0,0,0],
+         [1,0,0,0,0,0,0,0,1,0],
+         [0,0,0,0,1,0,0,0,0,0],
+         [0,0,0,0,0,0,0,1,0,0],
+         [0,1,1,0,0,0,0,0,0,0]]
