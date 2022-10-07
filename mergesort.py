6ee8a07e79abdd5e4f32f6a0dfc28a12c6feee33
Redstonerayy <antonpaulr@protonmail.com>
2022-10-07 17:58:01 +0200

    updated mergesort

--- mergesort.py
+++ mergesort.py
@@ -1,15 +1,13 @@
 import random
 
-from regex import P
+def gen(length, max):
+	return [random.randint(0,max) for i in range(length)]
 
-def gen(length):
-	return [random.randint(1,1000) for i in range(length)]
-
-def divide_and_conquer(array_ : list[int]) -> list[int]:
+def mergesort(array_ : list[int]) -> list[int]:
 	# only one element, it is min and max
-	if len(array_) < 2 :
+	if len(array_) == 1 :
 		return array_
-	# two elements, smaller is min, other max
+	# two elements, smaller to left side
 	elif len(array_) == 2:
 		if array_[0] < array_[1]:
 			return [array_[0], array_[1]]
@@ -18,31 +16,25 @@ def divide_and_conquer(array_ : list[int]) -> list[int]:
 	else:
 		# split array
 		pindex = 0
-		pivot = array_[pindex]
-		while True:
-			b1 = []
-			b2 = []
-			for num in array_:
-				if num < pivot:
-					b1.append(num)
-				else:
-					b2.append(num)
-			if not b1 and b2:
-				pindex += 1
-				#is sorted
-				if pindex == len(array_):
-					return array_
-				pivot = array_[pindex]
-				continue
-			break
+		pivot = array_[pindex]		
+		b1 = []
+		b2 = []
+		# sort based on pivot element
+		for num in array_:
+			if num < pivot:
+				b1.append(num)
+			else:
+				b2.append(num)
+		# if b1 is empty, the pivot is the smallest element. add it
+		# to b1, remove it from b2
+		if not b1:
+			b2.remove(pivot)
+			b1 = [pivot]
 
-		print(b1)
-		print(b2)
-		print("----")
-
-		p1 = divide_and_conquer(b1)
-		p2 = divide_and_conquer(b2)
+		p1 = mergesort(b1)
+		p2 = mergesort(b2)
 		# merge
 		return [*p1,*p2]
 
-print(divide_and_conquer([1,2,4,5,3,8,11,9]))
+print(mergesort([14,2,4,5,3,8,11,9]))
+print(mergesort(gen(100,1000)))
