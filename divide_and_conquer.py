21c06e5c1004917e7e401655d7f75e96746ebf1f
Redstonerayy <redstone.rayy@gmail.com>
2022-10-11 11:20:12 +0200

    starting quicksort

--- divide_and_conquer.py
+++ divide_and_conquer.py
@@ -3,7 +3,7 @@ import random
 def gen(length):
 	return [random.randint(1,1000) for i in range(length)]
 
-def divide_and_conquers(array_ : list[int]) -> tuple[int, int]:
+def divide_and_conquer(array_ : list[int]) -> tuple[int, int]:
 	# only one element, it is min and max
 	if len(array_) == 1:
 		return array_[0], array_[0]
@@ -16,11 +16,12 @@ def divide_and_conquers(array_ : list[int]) -> tuple[int, int]:
 	else:
 		# split array
 		# find min, max for each array
-		min1, max1 = divide_and_conquers(array_[:len(array_)//2])
-		min2, max2 = divide_and_conquers(array_[len(array_)//2:])
+		divider = len(array_)//2
+		min1, max1 = divide_and_conquer(array_[:divider])
+		min2, max2 = divide_and_conquer(array_[divider:])
 		# check which array has the large or smaller min, max
 		return min1 if min1 < min2 else min2, max1 if max1 > max2 else max2 
 
 liste = [25, 6,7,10,3,20,5,13,9,8]
-print(divide_and_conquers(liste))
-print(divide_and_conquers(gen(100)))
+print(divide_and_conquer(liste))
+print(divide_and_conquer(gen(100)))
---
+++ quicksort.py
@@ -0,0 +1,30 @@
+import random
+
+random.seed(4)
+
+def gen(length, max):
+	return [random.randint(0,max) for i in range(length)]
+
+def quicksort(array_, start, end):
+	pivot = array_[end]
+
+	i = start - 1
+
+	for j in range(end):
+		if array_[j] < array_[end]:
+			i += 1
+			temp = array_[j]
+			array_[j] = array_[i]
+			array_[i] = temp
+
+	temp = array_[end]
+	array_[end] = array_[i + 1]
+	array_[i + 1] = temp
+	print(array_)
+	print(start, i)
+	print(i + 2, end)
+	return i
+
+liste = gen(10, 10)
+print(liste)
+quicksort(liste, 0, len(liste) - 1)
