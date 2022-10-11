85ca84e8868379d2e61141a26c91f8bf0d7972a8
Redstonerayy <redstone.rayy@gmail.com>
2022-10-11 16:35:13 +0200

    quicksort working

--- quicksort.py
+++ quicksort.py
@@ -6,26 +6,30 @@ random.seed(4)
 def gen(length, max):
 	return [random.randint(0,max) for i in range(length)]
 
+def swap(array_, i1, i2):
+	temp = array_[i1]
+	array_[i1] = array_[i2]
+	array_[i2] = temp
+	return array_
+
 def quicksort(array_, start, end):
+	if start >= end:
+		return
 	pivot = array_[end]
 
 	i = start - 1
 
-	for j in range(end):
-		if array_[j] < array_[end]:
+	for j in range(start, end):
+		if array_[j] < pivot:
 			i += 1
-			temp = array_[j]
-			array_[j] = array_[i]
-			array_[i] = temp
+			swap(array_, i, j)
 
-	temp = array_[end]
-	array_[end] = array_[i + 1]
-	array_[i + 1] = temp
-	print(array_)
-	print(start, i)
-	print(i + 2, end)
-	return i
+	swap(array_, i + 1, end)
+
+	quicksort(array_, start, i)
+	quicksort(array_, i + 2, end)
 
 liste = gen(10, 10)
 print(liste)
 quicksort(liste, 0, len(liste) - 1)
+print(liste)
