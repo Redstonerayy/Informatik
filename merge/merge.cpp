9fcefc28ac6bfffc2b252bb27ee91fe5d03032ab
Redstonerayy <redstone.rayy@gmail.com>
2022-11-01 13:30:08 +0100

    added merge.cpp and comments

--- merge/merge.cpp
+++ merge/merge.cpp
@@ -28,18 +28,18 @@ std::vector<int> merge(std::vector<int> &v1, std::vector<int> &v2){
 	std::vector<int> result;
 	while(i < v1.size() && j < v2.size()){
 		if(v1[i] < v2[j]){
-			result.emplace_back(v1[i])
+			result.emplace_back(v1[i]);
 			++i;
 		} else {
-			result.emplace_back(v2[j])
+			result.emplace_back(v2[j]);
 			++j;
 		}
 	}
 
 	if(i < v1.size()){
-		result.insert(result.end(), v1.at(i), v1.end());
+		result.insert(result.end(), v1.begin() + i, v1.end());
 	} else {
-		result.insert(result.end(), v2.at(i), v2.end());
+		result.insert(result.end(), v2.begin() + j, v2.end());
 	}
 
 	return result;
@@ -47,11 +47,14 @@ std::vector<int> merge(std::vector<int> &v1, std::vector<int> &v2){
 
 int main(){
 	std::vector<int> l1 = gen(10);
+	std::sort(l1.begin(), l1.end());
 	std::vector<int> l2 = gen(10);
+	std::sort(l2.begin(), l2.end());
 	printvector(l1);
 	printvector(l2);
 	
 	std::vector<int> lg = merge(l1, l2);
+	printvector(lg);
 
 	return 0;
 }
--- merge/merge.py
+++ merge/merge.py
@@ -1,16 +1,22 @@
 import random
 
+# set seed so output is comparable
 random.seed(10)
 
+# generate random list, which is sorted
 def gen(length : int):
 	liste = [random.randint(0, 100) for i in range(length)]
 	liste.sort()
 	return liste
 
+# sort an array consisting of to sorted parts
+# return a new sorted combination of both
 def merge(_array, middle):
+	# initialize counters and output array
 	i = 0
 	j = middle
 	result = []
+	# compare and add elements
 	while i < middle and j < len(_array):
 		if _array[i] < _array[j]:
 			result.append(_array[i])
@@ -19,6 +25,7 @@ def merge(_array, middle):
 			result.append(_array[j])
 			j += 1
 
+	# add rest of the unfinished array
 	if i < middle:
 		result += _array[i:middle]
 	else:
@@ -26,6 +33,7 @@ def merge(_array, middle):
 
 	return result
 
+# testing
 l1 = gen(10)
 l2 = gen(10)
 lg = l1 + l2
