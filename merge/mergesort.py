07e68bb74fa13749972b1994e0cc0dbbab6a63c8
Redstonerayy <antonpaulr@protonmail.com>
2022-11-03 19:13:22 +0100

    added merge with O(n) space?

--- merge/mergesort.py
+++ merge/mergesort.py
@@ -1,4 +1,4 @@
-import random
+import random	
 
 # set seed so output is comparable
 random.seed(10)
@@ -19,24 +19,39 @@ def mergesort(_array, start, end):
 	elif end - start == 1:
 		if _array[start] > _array[end]:
 			swap(_array, start, end)
+		return
 	else:
 		# sort parts
 		middle = start + (end - start) // 2
 		mergesort(_array, start, middle)
 		mergesort(_array, middle + 1, end)
+		
 		# merge parts
+		merge = []
 		i = start
 		j = middle + 1
-		k = middle
-		while i < middle and j < end:
+		while i <= middle and j <= end:
 			if _array[i] < _array[j]:
+				merge.append(_array[i])
 				i += 1
 			else:
-				swap(_array, j, k)
+				merge.append(_array[j])
 				j += 1
-				k -= 1
+		
+		if i <= middle:
+			merge += _array[i:middle + 1]
+		else:
+			merge += _array[j:]
 
-liste = gen(4)
+		# replace in array
+		j = 0
+		for i in range(start, end + 1):
+			_array[i] = merge[j] 
+			j += 1
+
+		return 
+
+liste = gen(10)
 print(liste)
 mergesort(liste, 0, len(liste) - 1)
 print(liste)
